f3a5b27c00e37ca6148bb35642e57be7ca1c300e
maryk.december@gmail.com <maryk.december@gmail.com>
2015-05-07 21:43:23 -0500

    add onclick functionality

--- client/app.js
+++ client/app.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
     $(".teach").load(path + " .teach p");
     $(".research").load(path + " .research p");
     $(".if").load(path + " .if p");
+
+    $('.span1').click(function(){
+        $('.cat').parent().addClass('hidden');
+        $('.school').removeClass('hidden');
+    });
+
+    $('.span2').click(function(){
+        $('.cat').parent().addClass('hidden');
+        $('.work').removeClass('hidden');
+    });
+
+    $('.span3').click(function(){
+        $('.cat').parent().addClass('hidden');
+        $('.other').removeClass('hidden');
+    });
+
+
 });
+
+
