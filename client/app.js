0a812650b18b3e0f459563fbd42306a003c6b317
maryk.december@gmail.com <maryk.december@gmail.com>
2015-05-09 15:31:15 -0500

    initial commit

--- client/app.js
+++ client/app.js
@@ -1,64 +1,39 @@
-var path = "/views/resume.html";
+var resumeData;
+var template;
 
-$(document).ready(function(){
-//load resume items into index.html
-    $(".prime").load(path + " .prime p");
-    $(".macalester").load(path + " .macalester p");
-    $(".hola").load(path + " .hola p");
-    $(".teach").load(path + " .teach p");
-    $(".research").load(path + " .research p");
-    $(".if").load(path + " .if p");
-    $(".about").load(path + " .about p");
-
-    //hides resume content with name click
-    $('.name').click(function(){
-        $('.cat').parent().addClass('hidden');
-        $('.header').addClass('only');
-    });
-
-    //display info for school, work, talk
-    click_cat('.span1', '.school');
-    click_cat('.span2', '.work');
-    click_cat('.span3', '.talk');
-
-    // change color on document load
-    changeColor();
-    setInterval(changeColor, 5000);
-
-    //change color on window resize
-    $( window ).resize(function(){
-        changeColor();
-    });
-
-    //generate on click functions to show different resume sections
-    function click_cat (str1, str2) {
-        $(str1).click(function(){
-            $('.header').removeClass('only');
-            $('.cat').parent().addClass('hidden');
-            $(str2).removeClass('hidden');
-        });
+//get data
+$.ajax({
+    url: '/data',
+    success: function(response) {
+        resumeData = response.Resume;
+        addTemplate();
     }
-
-    //change color for all slash rows
-    function changeColor(){
-        randomSlash('.hi');
-        randomSlash('.footer');
-    }
-
-    //change color for a random visible child of a class
-    function randomSlash(str){
-        $(str).children().removeClass('select');
-        var children = ($(str).children(':visible').length);
-        var child = randomNumber(0, children-1);
-        $(str + " div:visible").eq(child).addClass('select');
-
-    }
-
-    //generate random number
-    function randomNumber(min, max) {
-            return Math.floor(Math.random() * (1 + max - min) + min);
-    }
-
 });
 
+//response.Resume.forEach(function(){
+//});
+
+//append templates to index
+function addTemplate(){
+    var item;
+    var el;
+    $.ajax({
+        url: '/template',
+        success: function(res){
+            template = res;
+            for (var i = 0; i < resumeData.length; i++){
+                $('.resume').append(template);
+                item = resumeData[i];
+                el = $('.resume').children().last();
+                el.addClass(item.type);
+                el.children('.name').html(item.name);
+            }
+        }
+    });
+}
+
+
+$(document).ready(function(){
+
+});
 
--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
-
 var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/data', function(request, response){
+    response.sendFile(path.join(__dirname, './public/assets/data/resume-data.json'));
+});
 
-app.get('/*', function(req, res){
-    var file = req.params[0] || 'views/index.html';
-    res.sendFile(path.join(__dirname, './public', file));
+app.get('/template', function(request, response){
+    response.sendFile(path.join(__dirname, './public/views/templates/category.html'));
+});
+
+app.get('/*', function(request, response){
+    var file = request.params[0] || 'views/index.html';
+    response.sendFile(path.join(__dirname, './public', file));
 });
 
 app.listen(app.get('port'), function(){
-    console.log('App is running on Port: ', app.get('port'));
+    console.log('running on port: ', app.get('port'));
 });
