9b67113a827003ee4992134c20dc35e3b790010f
maryk.december@gmail.com <maryk.december@gmail.com>
2015-05-09 16:12:31 -0500

    async functions in client app

--- client/app.js
+++ client/app.js
@@ -1,39 +1,55 @@
 var resumeData;
 var template;
 
-//get data
-$.ajax({
-    url: '/data',
-    success: function(response) {
-        resumeData = response.Resume;
-        addTemplate();
-    }
-});
-
-//response.Resume.forEach(function(){
-//});
-
-//append templates to index
-function addTemplate(){
-    var item;
-    var el;
+function getResume() {
     $.ajax({
-        url: '/template',
-        success: function(res){
-            template = res;
-            for (var i = 0; i < resumeData.length; i++){
-                $('.resume').append(template);
-                item = resumeData[i];
-                el = $('.resume').children().last();
-                el.addClass(item.type);
-                el.children('.name').html(item.name);
-            }
+        url: '/data',
+        success: function(response) {
+            resumeData = response.Resume;
+            getTemplate();
         }
     });
 }
 
+//append templates to index
+function getTemplate(){
+    $.ajax({
+        url: '/template',
+        success: function(res){
+            template = res;
+            addInfo();
+            }
+        });
+    }
+
+function addInfo(){
+    var item;
+    var el;
+    for (var i = 0; i < resumeData.length; i++) {
+        $('.resume').append(template);
+        item = resumeData[i];
+        el = $('.resume').children().last();
+        el.addClass(item.type);
+        el.children('.name').html(item.name);
+        el.children('.position').html(item.position);
+        el.children('.description').html(item.description);
+    }
+}
 
 $(document).ready(function(){
+    $('.span1').click(check);
 
 });
 
+function check(){
+    if (!resumeData){
+        getResume();
+        (console.log('this'));
+    } else if (!template) {
+        addTemplate();
+        console.log('no that');
+    } else {
+        addInfo();
+        console.log('nahhh');
+    }
+}
